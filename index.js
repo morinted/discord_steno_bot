9790e1a3417e5431475a301e192e4c68b09b83c6
Ted Morin <theodore.morin@clearwaterclinical.com>
2017-02-02 22:09:22 -0500

    Update comments

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 const Discord = require('discord.js')
 const stenoToBuffer = require('./steno.js').stenoToBuffer
-const token = require('./token').token
+const token = require('./token').token // Need a token.json with: { "token": "TOKEN HERE" }
 
 const bot = new Discord.Client()
-// the token of your bot - https://discordapp.com/developers/applications/me
 
-// the ready event is vital, it means that your bot will only start reacting to information
-// from Discord _after_ ready is emitted.
 bot.on('ready', () => {
   console.log('I am ready!')
 })
 
 const stenoGroups = /`([A-Z#\d\/ \*-]+)`/g
 
-// create an event listener for messages
 bot.on('message', message => {
-  // if the message is "ping",
   const content = message.content
   const channel = message.channel.name
   const summoned = channel === 'learners' || content.startsWith('!')
@@ -26,7 +21,6 @@ bot.on('message', message => {
     }
     console.log('Processing ', steno)
     const buffer = stenoToBuffer(steno)
-    // send "pong" to the same channel.
     if (buffer) {
       message.channel.sendFile(buffer)
     } else {
@@ -35,5 +29,4 @@ bot.on('message', message => {
   }
 })
 
-// log our bot in
 bot.login(token)
